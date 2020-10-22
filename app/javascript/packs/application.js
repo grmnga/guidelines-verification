9bd4d6ce9b558ac90c108049822da1d578bea738
Anna <anna@grmn.ga>
2020-10-22 21:52:56 +0500

    toggle select_subsection_blocks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,15 +26,21 @@ function disableButton() {
     this.style = "background: #AAAAAA; cursor: default; pointer-events: none;";
 }
 
+function toggle(el) {
+    el.style.display = (el.style.display == 'block') ? '' : 'block'
+}
+
 window.onload = function() {
     document.getElementById('start_full_checking').addEventListener('click', ShowMessage);
     document.getElementById('start_subsection_checking').addEventListener('click', ShowMessage);
     document.getElementById('show_select_subsection_button').addEventListener('click', function () {
-       document.getElementById('select_subsection_block').style = 'display: block';
+        toggle(document.getElementById('select_subsection_block'));
+       // document.getElementById('select_subsection_block').style = 'display: block';
     });
     document.getElementById('start_subsection_checking_2020').addEventListener('click', ShowMessage);
     document.getElementById('show_select_subsection_button_2020').addEventListener('click', function () {
-       document.getElementById('select_subsection_block_2020').style = 'display: block';
+        toggle(document.getElementById('select_subsection_block_2020'));
+       // document.getElementById('select_subsection_block_2020').style = 'display: block';
     });
 
     document.getElementById("start_full_checking").addEventListener("click", disableButton);
