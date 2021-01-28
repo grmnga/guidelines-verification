1a88f8ccd8e1769fca43c57d17efeae0bbb11159
Anna <anna@grmn.ga>
2021-01-28 18:47:25 +0500

    configure http headers

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Guidelines
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_dispatch.default_headers.merge!({
+                                                    'Access-Control-Allow-Origin' => '*',
+                                                    'Access-Control-Request-Method' => '*'
+                                                  })
   end
 end
