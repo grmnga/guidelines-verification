01f741d7070312b60aed7a4f1f33ffd6560fe937
Anna <anna@grmn.ga>
2021-01-28 14:57:16 +0500

    create job for table generation

--- app/jobs/generate_table_job.rb
+++ app/jobs/generate_table_job.rb
@@ -6,6 +6,9 @@ class GenerateTableJob < ApplicationJob
   def perform()
     puts 'should start bsm'
     GenerateBsmTable.start
+    bsm_table_html = IO.read('result.html')
+    bsm = OpopTableHtml.where('level': 'bsm')
+    bsm.update('html': bsm_table_html)
     GenerateTableJob.set(wait: 5.minutes).perform_later
   end
 end
